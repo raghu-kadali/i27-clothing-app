dc56e694ebd21edaccfba863f3f0e565bdd2c08a
hari <rakesh@gmail.com>
2026-05-05 22:54:58 +0530

    configure product and user ip and port to connect to other microserverscies

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://35.224.78.158:30001/api'
-const usersServiceDevHost = 'http://35.224.78.158:30722/api'
+const produtsServiceDevHost = 'http://136.113.77.108:31144/api'
+const usersServiceDevHost = 'http://136.113.77.108:32202/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
